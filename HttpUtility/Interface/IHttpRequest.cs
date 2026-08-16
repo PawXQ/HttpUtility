69291bc537f575144f006f483cded6c8e34848e3
PawXQ <pawxq@mail.com>
2026-08-17 01:07:14 +0800

    provision interface get put post patch delete

---
+++ HttpUtility/Interface/IHttpRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using HttpUtility.Model;
+
+namespace HttpUtility.Interface
+{
+    internal interface IHttpRequest
+    {
+        Task<string> GetAsync(string url);
+        Task<TResult> GetAsync<TResult>(string url, Dictionary<string, string> urlParam = null);
+        Task<string> PostAsync(string url, object input);
+        Task<TResult> PostAsync<TResult>(string url, object input, Dictionary<string, string> urlParam = null);
+        Task<TResult> PostAsync<TResult>(string url, MultipartFormDataContent input, Dictionary<string, string> urlParam = null);
+
+        Task<string> PatchAsync(string url, object input);
+        Task<TResult> PatchAsync<TResult>(string url, object input, Dictionary<string, string> urlParam = null);
+        Task<TResult> PatchAsync<TResult>(string url, MultipartFormDataContent input, Dictionary<string, string> urlParam = null);
+
+        Task<string> PutAsync(string url, object input);
+        Task<TResult> PutAsync<TResult>(string url, object input, Dictionary<string, string> urlParam = null);
+        Task<string> PutAsync(string url, HttpContent content);
+        Task<HttpResponseMessage> DeleteAsync(string url);
+        Task<DeleteResult> DeleteAsync(string url, Dictionary<string, string> urlParam = null);
+
+        String BaseUrl { set; }
+        String Token { set; get; }
+        //String SecretKey { set; get; }
+        //Credential Credential { set; }
+    }
+}
---
+++ HttpUtility/Utility/HttpRequest.cs
@@ -0,0 +1,194 @@
+﻿using HttpUtility.Interface;
+using HttpUtility.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpUtility.Utility
+{
+    internal class HttpRequest : IHttpRequest
+    {
+        private string _baseUrl;
+        public string BaseUrl { get => _baseUrl; set => _baseUrl = value; }
+        private string _token = null;
+        public string Token { get => _token; set => _token = value; }
+
+        private HttpClient httpClient;
+
+        public HttpRequest(string baseUrl, string token = null)
+        {
+            BaseUrl = baseUrl;
+
+            HttpClient httpClient = new HttpClient();
+
+            httpClient.BaseAddress = new Uri(baseUrl);
+
+            if (token != null) { httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}"); }
+        }
+
+        public async Task<string> GetAsync(string url)
+        {
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(url);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            string response = await responseMessage.Content.ReadAsStringAsync();
+
+            return response;
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string url, Dictionary<string, string> urlParam = null)
+        {
+            url = buildQueryString(url, urlParam);
+
+            string response = await GetAsync(url);
+
+            return JsonConvert.DeserializeObject<TResult>(response);
+        }
+
+        public async Task<string> PostAsync(string url, object input)
+        {
+            string reqBody = JsonConvert.SerializeObject(input);
+            HttpContent content = new StringContent(reqBody);
+
+            HttpResponseMessage responseMessage = await httpClient.PostAsync(url, content);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            string response = await responseMessage.Content.ReadAsStringAsync();
+
+            return response;
+        }
+
+        public async Task<TResult> PostAsync<TResult>(string url, object input, Dictionary<string, string> urlParam = null)
+        {
+            url = buildQueryString(url, urlParam);
+
+            string response = await PostAsync(url, input);
+
+            return JsonConvert.DeserializeObject<TResult>(response);
+        }
+
+        public Task<TResult> PostAsync<TResult>(string url, MultipartFormDataContent input, Dictionary<string, string> urlParam = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> PutAsync(string url, object input)
+        {
+            string reqBody = JsonConvert.SerializeObject(input);
+            HttpContent content = new StringContent(reqBody);
+
+            HttpResponseMessage responseMessage = await httpClient.PutAsync(url, content);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            string response = await responseMessage.Content.ReadAsStringAsync();
+
+            return response;
+        }
+
+        public async Task<TResult> PutAsync<TResult>(string url, object input, Dictionary<string, string> urlParam = null)
+        {
+            url = buildQueryString(url, urlParam);
+
+            string response = await PutAsync(url, input);
+
+            return JsonConvert.DeserializeObject<TResult>(response);
+        }
+
+        public async Task<string> PutAsync(string url, HttpContent content)
+        {
+            HttpResponseMessage responseMessage = await httpClient.PutAsync(url, content);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            string response = await responseMessage.Content.ReadAsStringAsync();
+
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            HttpResponseMessage responseMessage = await httpClient.DeleteAsync(url);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            return responseMessage;
+        }
+
+        public async Task<DeleteResult> DeleteAsync(string url, Dictionary<string, string> urlParam = null)
+        {
+            url = buildQueryString(url, urlParam);
+
+            HttpResponseMessage responseMessage = await httpClient.DeleteAsync(url);
+
+            return new DeleteResult
+            {
+                IsSuccess = responseMessage.IsSuccessStatusCode,
+                StatusCode = (int)responseMessage.StatusCode,
+                Message = responseMessage.ReasonPhrase.ToString(),
+            };
+        }
+
+        public async Task<string> PatchAsync(string url, object input)
+        {
+            string reqBody = JsonConvert.SerializeObject(input);
+
+            var patchMethod = new HttpMethod("PATCH");
+
+            var request = new HttpRequestMessage(patchMethod, url)
+            {
+                Content = new StringContent(reqBody, Encoding.UTF8, "application/json")
+            };
+
+            HttpResponseMessage responseMessage = await httpClient.SendAsync(request);
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            string response = await responseMessage.Content.ReadAsStringAsync();
+
+            return response;
+        }
+
+        public async Task<TResult> PatchAsync<TResult>(string url, object input, Dictionary<string, string> urlParam = null)
+        {
+            url = buildQueryString(url, urlParam);
+
+            string response = await PatchAsync(url, input);
+
+            return JsonConvert.DeserializeObject<TResult>(response);
+        }
+
+        public Task<TResult> PatchAsync<TResult>(string url, MultipartFormDataContent input, Dictionary<string, string> urlParam = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string buildQueryString(string url, Dictionary<string, string> urlParam)
+        {
+            if (urlParam == null) return url;
+
+            string parameter = "?";
+            url += parameter;
+
+            foreach (var kvp in urlParam)
+            {
+                url += $"{kvp.Key}={kvp.Value}&";
+            }
+
+            url = url.TrimEnd('&');
+
+            return url;
+        }
+
+
+    }
+}
